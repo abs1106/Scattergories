d2573de1b6a34f1ccf1332b958b38c1be0403ab5
Arwa Baig <arwabaig786@gmail.com>
2026-06-24 13:11:13 -0400

    first message

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const client_1 = require("./generated/prisma/client");
+const adapter_pg_1 = require("@prisma/adapter-pg");
+//allows uses of different libraries 
+const adapter = new adapter_pg_1.PrismaPg({ connectionString: process.env.DATABASE_URL });
+const prisma = new client_1.PrismaClient({ adapter });
+(async () => {
+    console.log('Prisma Client connected');
+    const cities = await prisma.cities.findMany();
+    console.log('Cities:', cities);
+    await prisma.$disconnect();
+})();
