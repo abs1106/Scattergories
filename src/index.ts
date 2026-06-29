15c517f394581c2062459856741e9c8b9e556c36
samnramos <samnramos22@gmail.com>
2026-06-29 10:30:58 -0400

    edited the index.ts to generate random room code

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,35 @@ const PORT = 5432;
 
 app.use(express.json());
 
+//Generating the random room code for the rooms with thec help of copilot 
+
+function generateRoomCode() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numbers = "0123456789";
+
+    let code = "";
+
+    //The Room code format will be ABCD123 etc 
+
+    for(let i = 0; i < 4; i ++) {
+        code +=
+        letters[Math.floor(Math.random() * letters.length)];
+    }
+
+    for(let i = 0; i < 3; i++) {
+        numbers[Math.floor(Math.random() * numbers.length)];
+    }
+
+    return code;
+
+}
+
 app.post('/start', async (req, res) => {
-
-
-    res.send("Root Route");
+    const roomCode = generateRoomCode();
+    res.json ({
+        message: "Room created.",
+        roomCode: roomCode
+    });
 });
+
+app.listen(5432);
